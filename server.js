e831735954f7f072d14ecf717be06dcc95fedac4
Deepak-yaryan <121033640+Deepak-yaryan@users.noreply.github.com>
2023-06-15 11:06:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 app.post('/merge', upload.array('pdfs', 2), async (req, res, next)=> {
     console.log((req.files).length);
     let d = await mergePdfs(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path));
-    res.redirect(`http://localhost:3000/static/${d}.pdf`)
+    res.redirect(`https://pdfimerger.onrender.com/static/${d}.pdf`)
     setTimeout(() => {
         fs.unlinkSync(`./public/${d}.pdf`);
         fs.unlinkSync(`./uploads/${req.files[0].filename}`);
